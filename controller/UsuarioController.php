297339d6612cdc0fec67ffbc1a9ccc3468581eb7
karenvieira12 <karen.v.albuquerque@gmail.com>
2023-09-14 19:12:53 -0300

    primeiro commit

--- controller/UsuarioController.php
+++ controller/UsuarioController.php
@@ -1,20 +1,42 @@
 <?php
+include_once "../conexao/Conexao.php";
+include_once "../model/Usuario.php";
+include_once "../dao/UsuarioDAO.php";
 
 $usuario = new Usuario();
 $usuariodao = new UsuarioDAO();
 
 $id =  filter_input_array(INPUT_POST);
 
-if(insset($_POST['cadastrar'])){
+if(isset($_POST['cadastrar'])){
     $usuario->setNome($d['nome']);
     $usuario->setSobrenome($d['sobrenome']);
     $usuario->setIdade($d['idade']);
     $usuario->setSexo($d['sexo']);
 
     $usuariodao->create($usuario);
+
+    header("Location: ../../");
+}
+
+else if(isset($_POST('editar'))){
+
+    $usuario->setNome($d['nome']);
+    $usuario->setSobrenome($d['sobrenome']);
+    $usuario->setIdade($d['idade']);
+    $usuario->setSexo($d['sexo']);
+
+    $usuariodao->update($usuario);
+
+    header("Location: ../../");
+
 }
 
 else if (isset($_GET['del'])){
     $usuario->setId($_GET['del']);
     $usuariodao->delete($usuario);
+
+    header("Location: ../../");
+}else{
+    header("Location: ../../");
 }
--- dao/UsuarioDAO.php
+++ dao/UsuarioDAO.php
@@ -1,75 +1,75 @@
 <?php
 
 class UsuarioDAO{
-    public function create(Usuario $usuario){
-        try{
-            $sql = "INSERT INTO usuario(nome,sobrenome,idade,sexo)
-            VALUES(:nome,:sobrenome,:idade,:sexo";
 
-            $p_sql = Conexao ::getConexao() ->prepare($sql);
+    public  function create(Usuario $usuario){
+        try{
+            $sql = "INSERT INTO usuario (nome,sobrenome,idade,sexo)
+            VALUES(:nome,:sobrenome,:idade,:sexo)";
+            
+            $p_sql = Conexao::getConexao()->prepare($sql);
             $p_sql ->bindValue(":nome",$usuario->getNome());
             $p_sql ->bindValue(":sobrenome",$usuario->getSobrenome());
             $p_sql ->bindValue(":idade",$usuario->getIdade());
             $p_sql ->bindValue(":sexo",$usuario->getSexo());
 
             return $p_sql->execute();
-        }catch (Exeption $e){
-            print "Erro ao inserir um usuário <br>" .$e. '<br>';
+        }catch(Exception $e){
+            print "Erro ao inserir um usuário <br>".$e.'<br>';
         }
-
     }
 
     public function read(){
         try{
             $sql = "SELECT * FROM usuario order by nome asc";
             $result = Conexao::getConexao()->query($sql);
-            $result = $result->fetchAll(PDO::FERCH_ASSOC);
+            $result = $result->fetchAll(PDO::FETCH_ASSOC);
             $f_lista = array();
-            foreach($lista as $1){
-                $f_lista[] = $this->listaUsuario($1);
+            foreach($lista as $l){
+                $f_lista[] = $this->listaUsuarios($l);
             }
 
             return $f_lista;
         }catch(Exception $e){
             print "Erro ao buscar usuário <br>".$e.'<br>';
         }
-    }catch
     }
     public function update(Usuario $usuario){
         try{
             $sql = "UPDATE usuario SET
 
-            nome = :nome;
-            sobrenome = :sobrenome;
-            idade = :idade;
-            sexo = :sexo
-
-            WHERE id = :id";
-         $p_sql = Conexao ::getConexao() ->prepare($sql);
-         $p_sql ->bindValue(":nome",$usuario->getNome());
-         $p_sql ->bindValue(":sobrenome",$usuario->getSobrenome());
-         $p_sql ->bindValue(":idade",$usuario->getIdade());
-         $p_sql ->bindValue(":sexo",$usuario->getSexo());
+                nome =:nome,
+                sobrenome =:sobrenome,
+                idade =:idade,
+                sexo =:sexo
+                
+                WHERE id =:id";
+             $p_sql = Conexao::getConexao()->prepare($sql);
+             $p_sql ->bindValue(":nome",$usuario->getNome());
+             $p_sql ->bindValue(":sobrenome",$usuario->getSobrenome());
+             $p_sql ->bindValue(":idade",$usuario->getIdade());
+             $p_sql ->bindValue(":sexo",$usuario->getSexo());
+             $p_sql ->bindValue(":id",$usuario->getId());
+ 
+             return $p_sql->execute();
+         }catch(Exception $e){
+             print "Erro ao atualizar um usuário <br>".$e.'<br>';
          
-         return $p_sql->execute();
-        }catch (Exeption $e){
-            print "Erro ao inserir um usuário <br>" .$e. '<br>';
-        }
         }
     }
-
-    public function delete (Usuario $usuario){
+    
+    public function delete(Usuario $usuario){
         try{
-            $sql = "DELETE FROM usuario WHERE id =:id";
-            $p_sql = Conexao ::getConexao()->prepare($sql);
+            $sql = "DELETE FROM usuario WHERE id = :id";
+            $p_sql = Conexao::getConexao()->prepare($sql);
             $p_sql->bindValue(":id",$usuario->getId());
             return $p_sql->execute();
-        }catch(Exeption$e){
+        }catch(Exception $e){
             print "Erro ao excluir um usuário <br>".$e.'<br>';
     }
     }
 
-    private function listaUsuario($row){
+    private function listaUsuarios($row){
         $usuario = new Usuario();
         $usuario->setId($row['id']);
         $usuario->setNome($row['nome']);
@@ -80,4 +80,7 @@ class UsuarioDAO{
         return $usuario;
 
     }
+
 }
+
+?>
