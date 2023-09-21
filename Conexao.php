7eae3b06f0f445ecc23f03e5d72ce39604ddbe2b
karenvieira12 <karen.v.albuquerque@gmail.com>
2023-09-21 10:33:02 -0300

    primeiro commit

--- Conexao.php
+++ Conexao.php
@@ -6,7 +6,7 @@ class conexao{
     }
     public static function getConexao(){
         if(!isset(self::$instance)){
-            self::$instance = new PDO('mysql;host=localhost;dbname=crud','root','senac',array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            self::$instance = new PDO('mysql;host=localhost;dbname=teste','root','senac',array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
             self::$instance ->setAtribute(PDO::ATTR_ERREMODE,POD::ERREMODE_EXCEPTION);
             self::$instance ->setAtribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
         }
--- index.php
+++ index.php
@@ -152,7 +152,9 @@ $usuariodao = new UsuarioDAO();
             </table>
         </div>
     </div>
-    
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.6.0/umd/popper.min.js" integrity="sha512-BmM0/BQlqh02wuK5Gz9yrbe7VyIVwOzD1o40yi1IsTjriX/NGF37NyXHfmFzIlMmoSIBXgqDiG1VNU6kB5dBbA==" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 </body>
 
